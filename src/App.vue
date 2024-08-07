10fdd1b81565b4b2580dddcaf46afd56d2010dd4
j0rem0rV2 <phantonJ0rem0rV2@proton.me>
2024-08-07 10:48:04 -0100

    commit 1

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="calculadora">
+    <h1>calculadora</h1>
+    <div  class="teclado">
+      <input type="number" placeholder="Número 1" />
+      <select>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+  h1{
+    font-size: 3em;
+  }
+  .calculadora{
+  text-align: center;
+  margin-top: 80px;
+  }
+  input, select {
+  margin: 10px;
+  padding: 5px;
+  font-size: 16px;
+}
+</style>
